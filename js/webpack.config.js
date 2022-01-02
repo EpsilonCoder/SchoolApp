5330b34230f18ae20e3dc43005407a28806866d0
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-01-02 16:33:14 +0000

    projet SchoolApp et Jest

---
+++ js/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  // Le fichier qui sert de point d'entrée et qui importe les différentes dépendances de l'application
+  entry: "./js/app.js",
+  // Le dossier et nom du fichier qui sera généré par Webpack après le build
+  // Webpack va donc intégrer l'ensemble des modules (dépendances) nécessaires dans un seul fichier dist/app.js
+  output: {
+    filename: "js/app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+
